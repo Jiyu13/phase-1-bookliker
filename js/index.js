d186ba37d8fce39a0262b9aa851c7ef7aa05a874
Ziru Zou <jiyucoding@yahoo.com>
2023-01-22 12:59:21 -0500

    fetch books and render each book in DOM

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", (event) => {
+    const listPanel = event.target.querySelector("#list-panel")
+    const ul = event.target.querySelector("#list")
+
+
+    function getBooks() {
+        return fetch("http://localhost:3000/books")  // return fetch
+        .then(response => response.json())
+    }
+
+    function renderBook(book) {
+        const li = event.target.createElement("li")
+        console.log(li)
+        li.textContent = book["title"]
+        ul.append(li)
+    }
+
+    getBooks().then(books => books.forEach(book => renderBook(book)))
+
+});
